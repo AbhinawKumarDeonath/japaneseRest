25170651fbfc19edccf91780ad18af36667a3c97
AbhinawKumarDeonath <abhinawdeonath30@gmail.com>
2024-09-25 22:19:04 +0530

    added more animation

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+
+var text_anim = gsap.timeline({
+    ease: "Power1.easeInOut"
+})
+
+text_anim.from(" .h1-name",{
+    duration:1,
+    y:"50%",
+    opacity:"0",
+    stagger:"0.3"
+})
+var img_anim = gsap.timeline({scrollTrigger:{
+
+    trigger:".top-picks-container",
+    start:"10% 80%",
+    end:"50% 50%",
+    scrub:true,
+    // markers:true,
+
+}})
+
+img_anim.from(" .top-img",{
+    duration:1.5,
+    y:"50%",
+    opacity:"0",
+    stagger:"0.5"
+})
+var about_text_anim = gsap.timeline({scrollTrigger:{
+
+    trigger:".about-section",
+    start:"30% 50%",
+    end:"50% 50%",
+    scrub:true,
+    // markers:true,
+
+}})
+
+about_text_anim.from(" .about-text",{
+    duration:1,
+    y:"50%",
+    opacity:"0",
+    stagger:"0.5",
+    ease:"powerInOut"
+})
+
+
+
+
 let mm = gsap.matchMedia();
 
 mm.add("(min-width: 800px)", () => {
@@ -14,7 +62,8 @@ mm.add("(min-width: 800px)", () => {
     tl.to("#hero-img", {
         top:"117%",
         right:"43%",
-        width:"13%"
+        width:"13%",
+         opacity:"0"
     })
     var tl2 = gsap.timeline({scrollTrigger:{
     
@@ -29,12 +78,16 @@ mm.add("(min-width: 800px)", () => {
     tl2.to("#susi-img", {
         top:"230%",
         right:"69%",
-        width:"13%"
+        width:"13%",
+        opacity:"1"
     })
 
 
 
 });
+
+
+
 
 mm.add("(max-width: 769px)", () => {
     var tl = gsap.timeline({scrollTrigger:{
@@ -50,7 +103,8 @@ mm.add("(max-width: 769px)", () => {
     tl.to("#hero-img", {
         top:"115vh",
         right:"9vw",
-        width:"18vh"
+        width:"18vh",
+        opacity:"0"
     })
     var tl2 = gsap.timeline({scrollTrigger:{
     
@@ -65,7 +119,8 @@ mm.add("(max-width: 769px)", () => {
     tl2.to("#susi-img", {
         top:"216vh",
         right:"51vw",
-        width:"40vw"
+        width:"40vw",
+        opacity:"1"
     })
 });
 
